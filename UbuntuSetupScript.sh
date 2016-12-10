dab889720f57db328a84a954c8362129230d90ce
Eurybus <wraitholyth@gmail.com>
2016-12-10 19:31:25 +0200

    Update UbuntuSetupScript.sh

--- UbuntuSetupScript.sh
+++ UbuntuSetupScript.sh
@@ -1,1 +1,19 @@
 #!/Bin/bash
+
+#INSTALL SCRIPT FOR UBUNTU 16.10
+
+#Define our package arrays
+util_packages=("htop" "screen" "unity-tweak-tool")
+main_packages=("atom" "")
+extra_packages=("" "")
+
+#Let's go user's home dir
+cd ~
+
+#Let's update first
+echo "Updating pre-installed packages";
+sudo apt-get update -qq
+
+echo "Installing utilities:";
+echo "${util_packages}";
+sudo apt-get install -yy ${util_packages[@]}
